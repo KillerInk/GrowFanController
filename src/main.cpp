674892c6dd1affc700cc2c65ddc582dfe5c52f9c
killerink <killerink@email.de>
2024-08-12 17:21:34 +0200

    add ability to set pwm range, different fan different range...

--- src/main.cpp
+++ src/main.cpp
@@ -6,45 +6,99 @@
 #include "config.h"
 #include "SPIFFS.h"
 #include "mdns.h"
+#include "Preferences.h"
+#include "Arduino_JSON.h"
 
 DFRobot_GP8403 dac(&Wire, 0x5F);
 uint16_t voltage = 0;
 uint16_t voltage1 = 0;
 AsyncWebServer *server;
-int minVoltage = 800;  // hz
-int maxVoltage = 1100; // hz
+Preferences preferences;
+int minVoltage = 800;    // hz
+int maxVoltage = 1100;   // hz
+int minVoltage1 = 0;     // hz
+int maxVoltage1 = 10000; // hz
+const char *prefNamespace = "Voltage";
 
-void applyspeed(int volt, int id,int val)
+void applyspeed(int volt, int id, int val)
 {
   if (val > 0)
+  {
+    if (id == 0)
     {
       u_int16_t s = minVoltage + (float)(maxVoltage - minVoltage) * ((float)val / 100);
       volt = s;
     }
-    else
-      volt = 0;
+    else if(id == 1)
+    {
+      u_int16_t s = minVoltage1 + (float)(maxVoltage1 - minVoltage1) * ((float)val / 100);
+      volt = s;
+    }
+  }
+  else
+    volt = 0;
 
-    log_i("set id:%i voltage to %i", id, volt);
-    dac.setDACOutVoltage(volt, id);
+  log_i("set id:%i voltage to %i", id, volt);
+  dac.setDACOutVoltage(volt, id);
+}
+
+void onGetSettings(AsyncWebServerRequest *request)
+{
+  JSONVar myObject;
+  myObject["fan0voltage"] = voltage;
+  myObject["fan0min"] = minVoltage;
+  myObject["fan0max"] = maxVoltage;
+  myObject["fan1voltage"] = voltage1;
+  myObject["fan1min"] = minVoltage1;
+  myObject["fan1max"] = maxVoltage1;
+  request->send(200,"text/json",JSON.stringify(myObject));
 }
 
 void onCmd(AsyncWebServerRequest *request)
 {
 
   String variable = request->arg("var");
-  String value = request->arg("val");
-  String ids = request->arg("id");
-  int val = value.toInt();
-  int id = ids.toInt();
 
-  log_i("set %s to %s", variable.c_str(), value.c_str());
-
-  if (variable == "speed" && val <= 100 && val >= 0)
+  if (variable == "speed")
   {
-    if(id == 0)
-      applyspeed(voltage,id,val);
+    String value = request->arg("val");
+    String ids = request->arg("id");
+    int val = value.toInt();
+    int id = ids.toInt();
+    if (val <= 100 && val >= 0)
+    {
+      log_i("set %s to %s", variable.c_str(), value.c_str());
+      if (id == 0)
+        applyspeed(voltage, id, val);
+      else if (id == 1)
+        applyspeed(voltage1, id, val);
+      request->send(200);
+    }
+    else
+      request->send(400);
+  }
+  else if (variable == "voltage")
+  {
+    String ids = request->arg("id");
+    String min = request->arg("min");
+    String max = request->arg("max");
+    int id = ids.toInt();
+    preferences.begin(prefNamespace, false);
+    if (id == 0)
+    {
+      minVoltage = min.toInt();
+      maxVoltage = max.toInt();
+      preferences.putInt("minv0", minVoltage);
+      preferences.putInt("maxv0", maxVoltage);
+    }
     else if (id == 1)
-      applyspeed(voltage1,id,val);
+    {
+      minVoltage1 = min.toInt();
+      maxVoltage1 = max.toInt();
+      preferences.putInt("minv1", minVoltage1);
+      preferences.putInt("maxv1", maxVoltage1);
+    }
+    preferences.end();
     request->send(200);
   }
   else
@@ -56,6 +110,14 @@ void setup()
   // put your setup code here, to run once:
   if (Serial.available())
     Serial.begin(115200);
+
+  preferences.begin(prefNamespace, false);
+  maxVoltage = preferences.getInt("maxv0", maxVoltage);
+  minVoltage = preferences.getInt("minv0", minVoltage);
+  maxVoltage1 = preferences.getInt("maxv1", maxVoltage1);
+  minVoltage1 = preferences.getInt("minv1", minVoltage1);
+  preferences.end();
+
   dac.begin(I2C_SDA, I2C_SCL);
 
   WiFi.setHostname("Esp32 FanController");
@@ -70,6 +132,7 @@ void setup()
   SPIFFS.begin();
   server = new AsyncWebServer(http_port);
   server->on("/cmd", HTTP_GET, onCmd);
+  server->on("/settings", HTTP_GET, onGetSettings);
   server->serveStatic("/", SPIFFS, "/www/").setDefaultFile("index.html");
   server->begin();
   log_i("Started Webserver on port %i", http_port);
