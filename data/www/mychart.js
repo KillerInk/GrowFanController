ce8c1661095952b7f234b4a57095c248d35654a3
killerink <killerink@email.de>
2025-02-03 11:55:48 +0100

    fix request bug when day was smaller < 10

--- data/www/mychart.js
+++ data/www/mychart.js
@@ -26,6 +26,8 @@ function getChartDataForDay(date, newdata) {
         if (month < 10)
             month = 0 + "" + month;
         let day = date.getDate();
+        if (day < 10)
+            day = 0 + "" + day;
         let hour = date.getHours();
         if (hour < 10)
             hour = 0 + "" + hour;
